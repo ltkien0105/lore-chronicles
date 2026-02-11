c657e779488f69cc77bd878861dd19a44952e8a5
skirnefta <ltkien0105@gmail.com>
2026-02-11 23:57:11 +0800

    feat: add font files and index for font exports

---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,5 @@
+import BeaufortforLOLBold from "./BeaufortforLOL-Bold.otf";
+import SpiegelBold from "./Spiegel-Bold.otf";
+import SpiegelRegular from "./Spiegel-Regular.otf";
+
+export { BeaufortforLOLBold, SpiegelBold, SpiegelRegular };
